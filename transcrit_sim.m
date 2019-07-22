31d5ab6299ed682002b7e6c1671ef11d9468f3f1
Jyler Menard (jmenard) <jmenard@ampc36.math.uwaterloo.ca>
2019-07-22 12:27:56 -0400

    working transcritical bif. sim

--- transcrit_sim.m
+++ transcrit_sim.m
@@ -1,62 +1,62 @@
-
-# Model parameters
-sigma = 0.1; # noise intensity
-bl = -1; # control parameter initial value
-bh = 0.2; # control parameter final value
-bcrit = 0; # bifurcation point (computed in Mathematica)
-x0 = 0.0; # intial condition (equilibrium value)
-
-
-# Initialise arrays to store single time-series data
-t = t0:dt:tmax;
-x = np.zeros(len(t))
-y = np.zeros(len(t))
-
-# Set up bifurcation parameter b, that increases linearly in time from bl to bh
-mb = (bh - bl)./tmax;
-intercept_b = bl;
-b = mb.*t + intercept_b;
-
-# Time at which bifurcation occurs
-tbif = (-bl.*tmax)./(bh - bl);
-
-## Implement Euler Maryuyama for stocahstic simulation
-
-# Set seed
-rng(101)
-
-# Initialise a list to collect trajectories
-list_traj_append = []
-print(len(t))
-t_0 = time.time()
+function finResults = transcrit_sim(t0, dt, tmax, numSims)
+	% Model parameters
+	sigma = 0.1; % noise intensity
+	bl = -1;	 % control parameter initial value
+	bh = 0.2; 	 % control parameter final value
+	bcrit = 0; 	 % bifurcation point
+	x0 = 0.0; 	 % intial condition (equilibrium value)
 
 
-# loop over simulations
-print('\nBegin simulations \n')
-for j in range(numSims):
-    
-    
-    # Create brownian increments (s.d. sqrt(dt))
-    dW_x_burn = np.random.normal(loc=0, scale=sigma*np.sqrt(dt), size = int(tburn/dt))
-    dW_x = np.random.normal(loc=0, scale=sigma*np.sqrt(dt), size = len(t))
-        
-    # Run burn-in period on x0
-    for i in range(int(tburn/dt)):
-        x0 = x0 + de_fun(x0,bl)*dt + dW_x_burn[i]
-        
-    # Initial condition post burn-in period
-    x[0]=x0
-    
-    # Run simulation
-    for i in range(len(t)-1):
-        x[i+1] = x[i] + de_fun(x[i],b.iloc[i])*dt + dW_x[i]
-            
-    # Store series data in a temporary DataFrame
-    data = {'Realisation number': (j+1)*np.ones(len(t)),
-                'Time': t,
-                'x': x}
-    df_temp = pd.DataFrame(data)
-    # Append to list
-    list_traj_append.append(df_temp)
-    
-print('Simulation '+str(j+1)+' complete')
+	% Initialise arrays to store single time-series data
+	t = t0:dt:tmax;
+	t = t'; % make column
+	tburn = 100; % burn-in period
+
+	x = [];%np.zeros(len(t))
+	y = [];%np.zeros(len(t))
+
+	% Set up bifurcation parameter b, that increases linearly in time from bl to bh
+	mb = (bh - bl)./tmax;
+	intercept_b = bl;
+	b = mb.*t + intercept_b;
+
+	% Time at which bifurcation occurs
+	tbif = (-bl.*tmax)./(bh - bl);
+
+	%% Implement Euler Maryuyama for stocahstic simulation
+
+	% Set seed
+	rng(101)
+
+	% Initialise a list to collect trajectories
+	disp(length(t));
+	list_traj_append = struct();
+
+	% loop over simulations
+	disp('\nBegin simulations \n')
+	for j = 1:1:numSims
+		
+		% Create brownian increments (s.d. sqrt(dt))
+		dW_x_burn = sigma.*sqrt(dt).*randn(fix(tburn/dt),1);
+		dW_x = sigma.*sqrt(dt).*randn(length(t));
+			
+		% Run burn-in period on x0
+		for i = 1:1:(fix(tburn./dt))
+			x0 = x0 + nform_transcrit(x0,bl).*dt + dW_x_burn(i,1);
+		end	
+		% Initial condition post burn-in period
+		x(1,1)=x0;
+		
+		% Run simulation
+		for i =1:1:(length(t)-1)
+			x(i+1,1) = x(i,1) + nform_transcrit( x(i,1), b(i,1) ).*dt + dW_x(i,1);
+		end
+
+		
+		% Append to list
+		list_traj_append.( strcat('sim_', num2str(j)) ) = [t, x, b];
+
+		disp( strcat('Simulation ',num2str(j),' complete') )
+	end
+	finResults = list_traj_append;
+end
